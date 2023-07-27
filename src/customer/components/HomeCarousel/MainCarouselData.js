788f8b2192a9b7d2700134be63730396c4a2462d
hunggahot <hunggahot@gmail.com>
2023-07-27 21:24:48 +0700

    carousel banner 1

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,22 @@
+export const mainCarouselData = [
+  {
+    image:
+      "//theme.hstatic.net/200000585159/1000942073/14/slide_1_img.jpg?v=204",
+    path: "women/clothing/women_dress",
+  },
+  {
+    image:
+      "//theme.hstatic.net/200000585159/1000942073/14/slide_3_img.jpg?v=204",
+    path: "women/clothing/ao_dai",
+  },
+  {
+    image:
+      "//theme.hstatic.net/200000585159/1000942073/14/collection_banner.jpg?v=204",
+    path: "women/clothing/women_dress",
+  },
+  {
+    image:
+      "//theme.hstatic.net/200000585159/1000942073/14/slide_2_img.jpg?v=204",
+    path: "women/clothing/uniform",
+  },
+];
