890d5a94b529f2124ed18ef58caf54cbb03ca886
hunggahot <hunggahot@gmail.com>
2023-08-01 19:44:30 +0700

    data test

--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -3,15 +3,23 @@ import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
 import AliceCarousel from 'react-alice-carousel';
 import { Button } from '@mui/material';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import { useState } from 'react';
 
 const HomeSectionCarousel = () => {
+    const [activeIndex, setActiveIndex] = useState(0);
+
     const responsive = {
         0: { items: 1 },
         720: { items: 3 },
         1024: { items: 5 },
     };
 
-    const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
+    const slidePrev = () => setActiveIndex(activeIndex - 1);
+    const slideNext = () => setActiveIndex(activeIndex + 1);
+
+    const syncActiveIndex = ({ item }) => setActiveIndex(item);
+
+    const items = [1, 1, 1, 1, 1, 1, 1, 1].map((item) => <HomeSectionCard />);
 
     return (
         <div className="border">
@@ -22,25 +30,31 @@ const HomeSectionCarousel = () => {
                     responsive={responsive}
                     disableDotsControls
                     infinite
+                    onSlideChanged={syncActiveIndex}
+                    activeIndex={activeIndex}
                 />
-                <Button
-                    variant="contained"
-                    className="z-50 bg-white"
-                    sx={{
-                        position: 'absolute',
-                        top: '8rem',
-                        right: '0rem',
-                        transform: 'translateX(50%) rotate(90deg)',
-                        bgcolor: 'white',
-                    }}
-                    aria-label="next"
-                >
-                    <KeyboardArrowLeftIcon
-                        sx={{ transform: 'rotate(90deg)', color: 'black' }}
-                    />
-                </Button>
+                {activeIndex !== items.length - 5 && (
+                    <Button
+                        variant="contained"
+                        className="z-50 bg-white"
+                        onClick={slideNext}
+                        sx={{
+                            position: 'absolute',
+                            top: '8rem',
+                            right: '0rem',
+                            transform: 'translateX(50%) rotate(90deg)',
+                            bgcolor: 'white',
+                        }}
+                        aria-label="next"
+                    >
+                        <KeyboardArrowLeftIcon
+                            sx={{ transform: 'rotate(90deg)', color: 'black' }}
+                        />
+                    </Button>
+                )}
 
                 <Button
+                    onClick={slidePrev}
                     variant="contained"
                     className="z-50 bg-white"
                     sx={{
---
+++ src/data/women_aodai.js
@@ -0,0 +1,274 @@
+export const women_aodai = [
+    {
+        imageUrl:
+            'https://pubcdn.ivymoda.com/files/product/thumab/1400/2023/05/17/b530a02260b03cecd01b1ace2210a30d.jpg',
+        brand: 'Senora',
+        title: 'ÁO SƠ MI LỤA THÊU HOA',
+        color: 'Hồng tím',
+        discountedPrice: 623000,
+        price: 890000,
+        discountPercent: 30,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Một thiết kế áo với chất liệu lụa nhẹ nhàng cùng chi tiết thêu hoa giúp nàng trở nên yêu kiều hơn. Áo cổ đức thời thượng cùng khuy cài kim loại tạo điểm nhấn. Mix áo cùng chân váy hoặc quần đều là sự lựa chọn hoàn hảo cho nàng đi làm và đi chơi.',
+    },
+
+    {
+        imageUrl:
+            'https://pubcdn.ivymoda.com/files/product/thumab/1400/2023/07/06/6a2c40f49c0c6a781467d5709435f9d8.jpg',
+        brand: 'Senora',
+        title: 'SET BỘ TUYSI PHỐI BLAZER',
+        color: 'Xanh Táo',
+        discountedPrice: 1316000,
+        price: 1880000,
+        discountPercent: 30,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Thiết kế set bộ công sở đáp ứng đủ tình thần thanh lịch, hiện đại nhưng vẫn thật trẻ trung và năng động. Bản phối là sự kết hợp của mẫu áo blazer cách điệu với phần cầu vai bồng nhẹ, thiết kế 4 khuy kiểu cách bên cạnh dáng chân A nhẹ, hách dáng cũng như che khuyết điểm cực tốt. Nàng vừa có thể diện set bộ đi làm, đi họp hay đến những nơi vui chơi, gặp mặt bạn bè. ',
+    },
+
+    {
+        imageUrl:
+            'https://pubcdn.ivymoda.com/files/product/thumab/1400/2023/07/20/0e653008a81c21dd21570acbe2c439e0.JPG',
+        brand: 'Senora',
+        title: 'ÁO THUN ĐÍNH REN',
+        color: 'Trắng ngà',
+        discountedPrice: 325000,
+        price: 650000,
+        discountPercent: 50,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            '- Áo thun cổ tròn, tay ngắn, phần vai đính ren hoa nữ tính.- Chất liệu thun co dãn, thấm hú mồ hôi tốt mang lại cảm giác thoáng mát phfu hợp với thời tiết Việt Nam. - Kết hợp cùng jeans mang lại sự khỏe khoắn, phù hợp mặc hằng ngày. - Kết hợp với chân váy đồng bộ mang lại vẻ ngoài sang trọng, lịch sự phù hợp diện đi làm, gặp gỡ khách hàng và những sựu kiện quan trọng.',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+
+    {
+        imageUrl: '',
+        brand: '',
+        title: '',
+        color: '',
+        discountedPrice: 499,
+        price: 1499,
+        discountPercent: 66,
+        size: [
+            {
+                name: 'S',
+                quantity: 20,
+            },
+            {
+                name: 'M',
+                quantity: 30,
+            },
+            {
+                name: 'L',
+                quantity: 50,
+            },
+        ],
+        quantity: 100,
+        topLabelCategory: 'Women',
+        secondLabelCategory: 'Clothing',
+        thirdLabelCategory: 'women_aodai',
+        description:
+            'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod ab consequatur fugit provident sit corporis, tempore ipsam ullam blanditiis necessitatibus perferendis aut labore culpa explicabo, eveniet aliquam nihil minima sint?',
+    },
+];
