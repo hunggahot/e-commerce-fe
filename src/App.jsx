b2f11c1be1008988b61fc9d1a75d630355377461
hunggahot <hunggahot@gmail.com>
2023-08-12 08:43:33 +0700

    Cart Item

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
+import Cart from './customer/components/Cart/Cart';
 
 function App() {
   return (
@@ -12,7 +13,8 @@ function App() {
       <div>
         {/* <HomePage /> */}
         {/* <Product /> */}
-        <ProductDetails />
+        {/* <ProductDetails /> */}
+        <Cart />
       </div>
       <Footer />
     </div>
