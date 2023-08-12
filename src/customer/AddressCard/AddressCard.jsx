49acb58c20c07b02337f39894e2c3c47f297392c
hunggahot <hunggahot@gmail.com>
2023-08-12 10:49:45 +0700

    Address Card

--- src/customer/AddressCard/AddressCard.jsx
+++ src/customer/AddressCard/AddressCard.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const AddressCard = () => {
-  return <div>AddressCard</div>;
+  return (
+    <div>
+      <div className="space-y-3">
+        <p className="font-semibold">Quan 1</p>
+        <p>Ho Chi Minh, 70000</p>
+        <div className="space-y-1">
+          <p className="font-semibold">Phone Number</p>
+          <p>0362282969</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddressCard;
