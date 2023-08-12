e7676b4d94476a36d762130a1397291b8b703401
hunggahot <hunggahot@gmail.com>
2023-08-12 10:57:26 +0700

    Order Summary

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Footer from './customer/components/Footer/Footer';
 import Product from './customer/components/Product/Product';
 import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 import Cart from './customer/components/Cart/Cart';
-import Checkout from './customer/Checkout/Checkout';
+import Checkout from './customer/components/Checkout/Checkout';
 
 function App() {
   return (
