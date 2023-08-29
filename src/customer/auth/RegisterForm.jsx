c64757e961b265857945e767a4a226edc54bb1a2
hunggahot <hunggahot@gmail.com>
2023-08-29 11:55:23 +0700

    register form

--- src/customer/auth/RegisterForm.jsx
+++ src/customer/auth/RegisterForm.jsx
@@ -1,8 +1,21 @@
-import { Grid, TextField } from '@mui/material';
+import { Grid, TextField, Button } from '@mui/material';
 import React from 'react';
 
 const RegisterForm = () => {
-  const handleSubmit = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+
+    const userData = {
+      firstName: data.get('firstName'),
+      lastName: data.get('lastName'),
+      email: data.get('email'),
+      password: data.get('password'),
+    };
+
+    console.log('userData ', userData);
+  };
 
   return (
     <div>
@@ -17,6 +30,50 @@ const RegisterForm = () => {
               fullWidth
               autoComplete="given-name"
             />
+          </Grid>
+          <Grid item xs={12} sm={6}>
+            <TextField
+              required
+              id="lastName"
+              name="lastName"
+              label="Last Name"
+              fullWidth
+              autoComplete="given-name"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="email"
+              name="email"
+              label="Email"
+              fullWidth
+              autoComplete="email"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              required
+              id="password"
+              name="password"
+              label="Password"
+              fullWidth
+              autoComplete="password"
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button
+              className="w-full"
+              type="submit"
+              variant="contained"
+              size="large"
+              sx={{ padding: '.8rem 0' }}
+            >
+              Register
+            </Button>
           </Grid>
         </Grid>
       </form>
