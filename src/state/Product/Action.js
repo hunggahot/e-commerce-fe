8b19f0cce137fc86c112ba4aaf98ee240be45cf4
hunggahot <hunggahot@gmail.com>
2023-08-31 09:06:31 +0700

    state update

--- src/state/Product/Action.js
+++ src/state/Product/Action.js
@@ -1,4 +1,18 @@
+import { colors } from '@mui/material';
+import {
+  FIND_PRODUCTS_FAILURE,
+  FIND_PRODUCTS_REQUEST,
+  FIND_PRODUCTS_SUCCESS,
+  FIND_PRODUCT_BY_ID_FAILURE,
+  FIND_PRODUCT_BY_ID_REQUEST,
+  FIND_PRODUCT_BY_ID_SUCCESS,
+} from './ActionType';
+
+import { api } from '../../config/apiConfig';
+
 export const findProducts = (reqData) => async (dispatch) => {
+  dispatch({ type: FIND_PRODUCTS_REQUEST });
+
   const {
     color,
     sizes,
@@ -11,4 +25,28 @@ export const findProducts = (reqData) => async (dispatch) => {
     pageNumber,
     pageSize,
   } = reqData;
+
+  try {
+    const { data } = api.get(
+      `/api/v1/products/color=${colors}&size=${sizes}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`,
+    );
+
+    dispatch({ type: FIND_PRODUCTS_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: FIND_PRODUCTS_FAILURE, payload: error.message });
+  }
+};
+
+export const findProductById = (reqData) => async (dispatch) => {
+  dispatch({ type: FIND_PRODUCT_BY_ID_REQUEST });
+
+  const { productId } = reqData;
+
+  try {
+    const { data } = api.get(`/api/v1/products/id/${productId}`);
+
+    dispatch({ type: FIND_PRODUCT_BY_ID_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: FIND_PRODUCT_BY_ID_FAILURE, payload: error.message });
+  }
 };
