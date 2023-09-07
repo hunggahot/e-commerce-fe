8e80e4d16664364e14365e77e7e723f10f543cc6
hunggahot <hunggahot@gmail.com>
2023-09-07 19:47:39 +0700

    Admin Dashboard UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.scss';
 import { Route, Routes } from 'react-router-dom';
 import CustomerRouters from './routers/CustomerRouters';
+import AdminRouters from './routers/AdminRouters';
 
 function App() {
   return (
     <div className="">
       <Routes>
         <Route path="/*" element={<CustomerRouters />} />
+        <Route path="/admin/*" element={<AdminRouters />} />
       </Routes>
     </div>
   );
---
+++ src/admin/components/Achivement.jsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+import React from 'react';
+
+const TrignleImg = styled('img')({
+  right: 0,
+  bottom: 0,
+  height: 170,
+  position: 'absolute',
+});
+
+const TrophyImg = styled('img')({
+  right: 36,
+  bottom: 20,
+  height: 98,
+  position: 'absolute',
+});
+
+const Achivement = () => {
+  return (
+    <Card
+      className=""
+      sx={{ position: 'relative', bgcolor: '#242B2E', color: 'white' }}
+    >
+      <CardContent>
+        <Typography variant="h6" sx={{ letterSpacing: '.25px' }}>
+          E-Commerce
+        </Typography>
+        <Typography variant="body2">Congratulations</Typography>
+        <Typography variant="h5" sx={{ my: 3.1 }}>
+          420.8k
+        </Typography>
+
+        <Button size="small" variant="contained">
+          View Sales
+        </Button>
+
+        <TrignleImg src=""></TrignleImg>
+        <TrophyImg src="https://static.vecteezy.com/system/resources/previews/009/315/016/non_2x/winner-trophy-in-flat-style-free-png.png" />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Achivement;
